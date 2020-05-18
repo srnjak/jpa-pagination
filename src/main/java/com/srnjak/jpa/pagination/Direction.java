0ce9b9ab3c68e69792ea7a121b694bc7be16bc4c
Grega Krajnc <grega.krajnc@srnjak.com>
2020-05-18 23:44:39 +0200

    Improved the Direction enum with the "oposite" method.

--- src/main/java/com/srnjak/jpa/pagination/Direction.java
+++ src/main/java/com/srnjak/jpa/pagination/Direction.java
@@ -1,9 +1,22 @@
 package com.srnjak.jpa.pagination;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Direction where to search (i.e. in keyset pagination).
  */
 public enum Direction {
     BACKWARD,
     FORWARD;
+
+    private static final Map<Direction, Direction> OPOSITE_MAP = new HashMap<>();
+    static {
+        OPOSITE_MAP.put(BACKWARD, FORWARD);
+        OPOSITE_MAP.put(FORWARD, BACKWARD);
+    }
+
+    public Direction oposite() {
+        return OPOSITE_MAP.get(this);
+    }
 }
